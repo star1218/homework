b9f371375423a6a64e127fe706dc4b79c021e8ce
igork <igorkaaristocrat@gmail.com>
2024-02-20 23:01:34 +0200

    commit

--- main.py
+++ main.py
@@ -1,2 +1,52 @@
-#чистовик
-1
+# try: #задание 1
+#     massage="hello123world33"
+#     alpha_count=0
+#     digit_count=0
+#     for sinvol in massage:
+#         if sinvol.isalpha():
+#             alpha_count +=1
+#         elif sinvol.isdigit():
+#             digit_count +=1
+#         result=f" Букв: {alpha_count} цифр:{digit_count}"
+#     print(result)
+# except ValueError:
+#     print("Ведите правильное значение")
+# except Exception as error:
+#     print(error)
+# try: #задание4
+#     sentence="Hello world"
+#     print(sentence[2:])
+#     print(sentence[:-2])
+#     print(sentence[:-5])
+#     print(sentence[:-2])
+#     print(sentence[::2])
+#     print(sentence[1::2])
+#     print(sentence[::-1])
+#     print(sentence[-1::-2])
+#     print(len(sentence))
+# except ValueError:
+#     print("Invalid value")
+# except Exception as error:
+#     print(error)
+# try: #задание 2
+#     massage =input("Ведите рядок:")
+#     simvol=input("Символ для поиска:")
+#     count=massage.count(simvol)
+#     print("Повторений символов:",count)
+# except ValueError:
+#     print("Ведите правильное значение")
+# except Exception as error:
+#     print(error)
+
+# try: задание 3
+#     text = "My Home Work"
+#     print(text)
+#     text = text.replace("My Home", "Hello", 1)
+#     print(text)
+# except ValueError:
+#     print("Ведите правильное значение")
+# except Exception as error:
+#     print(error)
+
+
+
